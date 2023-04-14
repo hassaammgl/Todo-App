b2a73d21accb83b0517cfe437b9eb22b28b7eee5
Hassaam Mehtab <h455a4mmehtab@gmail.com>
2023-04-15 03:04:18 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { TodoWrapper } from './components/TodoWrapper'
+import './App.scss'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <TodoWrapper />
+    </div>
+  )
+}
+
+export default App
